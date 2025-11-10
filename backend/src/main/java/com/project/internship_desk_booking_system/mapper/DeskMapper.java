ace2048d4cb9b9b3c5eefcd047b95ea2951a2ec9
ifantaziu <roscairina012@gmail.com>
2025-11-10 16:34:52 +0200

    defing desk naming_bug fixing

--- backend/src/main/java/com/project/internship_desk_booking_system/mapper/DeskMapper.java
+++ backend/src/main/java/com/project/internship_desk_booking_system/mapper/DeskMapper.java
@@ -9,14 +9,13 @@ public class DeskMapper {
     public DeskDto toDto(Desk desk) {
         return new DeskDto(
                 desk.getId(),
-                desk.getZone().getZoneAbv()+desk.getDeskName(),
+                desk.getZone().getZoneAbv()+"-"+desk.getDeskName(),
                 desk.getZone().getId(),
                 desk.getType(),
                 desk.getStatus(),
                 desk.getIsTemporarilyAvailable(),
                 desk.getTemporaryAvailableFrom(),
-                desk.getTemporaryAvailableUntil(),
-                false
+                desk.getTemporaryAvailableUntil()
         );
     }
 }
